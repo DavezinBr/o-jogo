0d9b5e447e142e946d49e9a25e86d577fee192a6
DavezinBr <121894689+DavezinBr@users.noreply.github.com>
2023-10-26 15:00:15 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,16 +1,18 @@
 //crie as variaveis dos seus personagens aqui
 var box;
-
+var areia;
 
 function preload() {
-  //carregue as imagens dos personagens aqui
+  boxImg = loadImage("James das Frutas (2).png");
+  areiaImg = loadImage("areia do Projeto.jpg")
 }
 
 function setup() {
-  createCanvas(800, 400);
+  createCanvas(windowWidth, windowHeight);
   //crie os sprites aqui
   box = createSprite(200, 200, 50, 80);
-  box.shapeColor = "green";
+  box.addImage(boxImg)
+  
 
 
 
@@ -34,8 +36,27 @@ function controles() {
   }
 }
 
+//super velocidade
+/*function controles() {
+  if (keyDown("w")) {
+    box.y = box.y - 12;
+  }
+
+  if (keyDown("s")) {
+    box.y = box.y + 12;
+  }
+
+  if (keyDown("a")) {
+    box.x = box.x - 12;
+  }
+
+  if (keyDown("d")) {
+    box.x = box.x + 12;
+  }
+}*/
+
 function draw() {
-  background(255, 255, 255);
+  background(areiaImg);
   controles()
   drawSprites();
 }
